e8cedc818e6bfbe632f4f427dfadeb6046694e9e
osicrat <tarcisofera@gmail.com>
2026-03-12 09:29:24 -0300

    Delete webos directory

--- webos/vendor/hls.js
+++
@@ -1,7 +0,0 @@
-window.Hls = window.Hls || class {
-  static isSupported() { return !!window.MediaSource; }
-  attachMedia(video) { this.video = video; }
-  loadSource(url) { if (this.video) { this.video.src = url; this.video.play?.(); } }
-  on() {}
-  destroy() {}
-};
